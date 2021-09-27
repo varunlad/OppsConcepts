10fc876b9eb69a6a888263d35d08e4b2366b4006
varun <varunlad5@gmail.com>
2021-09-27 20:57:26 +0530

    [varun] Polymorphism Method OverRide OPPS Concept

--- OPPSPracticeProblems/Program.cs
+++ OPPSPracticeProblems/Program.cs
@@ -6,32 +6,38 @@ using System.Threading.Tasks;
 
 namespace OPPSPracticeProblems
 {
-    class Vehicle  // base class (parent) 
+    class Animal  // Base class (parent) 
     {
-        public string brand = "BMW";  // Vehicle field
-        public void honk()             // Vehicle method 
+        public virtual void animalSound()
         {
-            Console.WriteLine("PEE PEP!!!");
+            Console.WriteLine("The animal makes a sound");
         }
     }
-
-    class Car : Vehicle  // derived class (child)
+    class Pig : Animal  // Derived class (child) 
     {
-        public string modelName = "Mustang";  // Car field
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
     }
-
+    class Dog : Animal  // Derived class (child) 
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says: bow wow");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            // Create a myCar object
-            Car myCar = new Car();
+            Animal myAnimal = new Animal();  // Create a Animal object
+            Animal myPig = new Pig();  // Create a Pig object
+            Animal myDog = new Dog();  // Create a Dog object
 
-            // Call the honk() method (From the Vehicle class) on the myCar object
-            myCar.honk();
-
-            // Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
-            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
             Console.ReadLine();
         }
     }
